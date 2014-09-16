16468a3fdd47d019337ce4dc2f3aeb336b7bf0a7
valik35 <av.val.100@mail.ru>
2014-09-16 07:02:14 +0300

    add class Logic

---
+++ CurseSolution/GestureCam/Logic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video.DirectShow;
+
+namespace GestureCam
+{
+    class Logic
+    {
+        FilterInfoCollection videodevices;
+        VideoCaptureDevice videoSource;
+
+        public Logic()
+        {
+            videodevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = new VideoCaptureDevice(videodevices[0].MonikerString);
+
+            videoSource.NewFrame += videoSource_NewFrame;
+            videoSource.Start();
+        }
+
+        void videoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            
+        }
+    }
+}
--- CurseSolution/GestureCam/Program.cs
+++ CurseSolution/GestureCam/Program.cs
@@ -10,6 +10,7 @@ namespace GestureCam
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
